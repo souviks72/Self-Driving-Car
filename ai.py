3b40d71610d85896579c009b641ae792879a22c3
souviks72 <souvikmandalvit2016@gmail.com>
2020-03-22 11:37:19 +0530

    First commit: Experience replay being implemented

---
+++ ai.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 12 16:41:11 2020
+
+@author: Souvik M
+"""
+
+import numpy as np
+import random
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.autograd as autograd
+from torch.autograd import Variable
+
+class Network(nn.Module):
+    
+    def __init__(self,input_size,nb_action):
+        super(Network,self).__init__()
+        self.input_size = input_size
+        self.nb_action = nb_action
+        self.fc1 = nn.Linear(input_size,30)
+        self.fc2 = nn.Linear(30,nb_action)
+    
+    def forward(self,state):
+        x = F.relu(self.fc1(state))
+        q_values = self.fc2(x)
+        return q_values
+    
+class ReplayMemory(object):
+    
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.memory = []
+
+    def push(self,event):
+        self.memory.append(event) 
+        if len(self.memory) > self.capacity:
+            del self.memory[0]
+    
+    def sample(self,batch_size):
+        samples = zip(*random.sample(self.memory,batch_size))
+        return map(lambda x: Variable(torch.cat(x,0)),samples)
+    
